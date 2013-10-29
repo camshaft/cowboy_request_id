b8b482bd38d278cad71148ab884bb298bc893221
Cameron Bytheway <cameron@theflokk.com>
2013-10-29 11:19:13 -0700

    initial commit

---
+++ src/cowboy_request_id.erl
@@ -0,0 +1,32 @@
+-module(cowboy_request_id).
+
+-export([execute/2]).
+-export([init/0]).
+-export([init/1]).
+
+-export([get/1]).
+
+execute(Req, Env) ->
+  Fun = init([]),
+  Fun(Req, Env).
+
+init() ->
+  init([]).
+
+init(Options) ->
+  Header = fast_key:get(header, Options, <<"x-request-id">>),
+  Generator = fast_key:get(generator, Options, fun uuid:v4/0),
+  fun (Req, Env) ->
+    {RequestID, Req3} = case cowboy_req:header(Header, Req) of
+      {undefined, Req2} ->
+        {Generator(), Req2};
+      {ID, Req2} ->
+        {ID, Req2}
+    end,
+    Req4 = cowboy_req:set_meta(request_id, RequestID, Req3),
+    {Req4, Env}
+  end.
+
+get(Req) ->
+  {RequestID, _} = cowboy_req:meta(request_id, Req),
+  RequestID.
